d1a73d9ff6b7196e72729d4209a0ad27080382fd
kiwidom <dominic@mnemonics.co.nz>
2021-02-16 19:56:22 +1300

    Update broadleaf_garage.ino

--- broadleaf_garage/broadleaf_garage.ino
+++ broadleaf_garage/broadleaf_garage.ino
@@ -30,7 +30,7 @@
  *
  *
  */
- String __version__ = "$Revision: 3 $";
+ String __version__ = "$Revision: 4 $";
 // Load Wi-Fi and NTP and UDP libraries
 #include <NTPClient.h>
 #include <ESP8266WiFi.h>
@@ -342,142 +342,144 @@ String getState()
  */
 void checkForClient()
 {
-  WiFiClient client = server.available();   // Listen for incoming clients
+	WiFiClient client = server.available();   // Listen for incoming clients
 
-  if (client) 
-  {                                         // If a new client connects,
-    String currentLine = "";                // make a String to hold incoming data from the client
-    currentTime = millis();
-    previousTime = currentTime;
+	if (client) 
+	{                                         // If a new client connects,
+	    String currentLine = "";                // make a String to hold incoming data from the client
+	    currentTime = millis();
+	    previousTime = currentTime;
        
-    while (client.connected() && currentTime - previousTime <= timeoutTime) 
-    {                                       // loop while the client's connected
-      currentTime = millis();         
-      if (client.available())
-      {                                     // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        header += c;
-        if (c == '\n') 
-         {                                  // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) 
-          {
-            doorClosed = digitalRead(closedSwitch);
-            doorOpen = digitalRead(openSwitch);
-            
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
+	    while (client.connected() && currentTime - previousTime <= timeoutTime) 
+	    {                                       // loop while the client's connected
+			currentTime = millis();         
+			if (client.available())
+			{                                     // if there's bytes to read from the client,
+				char c = client.read();             // read a byte, then
+				header += c;
+				// if the byte is a newline character
+				if (c == '\n') 
+	 			{                                  
+					// if the current line is blank, you got two newline characters in a row.
+					// that's the end of the client HTTP request, so send a response:
+					if (currentLine.length() == 0) 
+					{
+						 // establish current position for decision logic below
+						doorClosed = digitalRead(closedSwitch);
+						doorOpen = digitalRead(openSwitch);
 
-            if (header.indexOf("GET /pulse") >= 0) 
-            {
-              activate();
-              addLogEntry("Client request for PULSE");
-            }
+						if (header.indexOf("GET /pulse") >= 0) 
+						{
+						  activate();
+						  addLogEntry("Client request for PULSE");
+						}
 
-            if (  (header.indexOf("GET /close") >= 0) && doorOpen == LOW  ) 
-            {
-              activate();
-              addLogEntry("Client request for CLOSE");
-            }
+						if (  (header.indexOf("GET /close") >= 0) && doorOpen == LOW  ) 
+						{
+						  activate();
+						  addLogEntry("Client request for CLOSE");
+						}
 
-            if (  (header.indexOf("GET /open") >= 0) && doorClosed == LOW   )
-            {
-              activate();
-              addLogEntry("Client request for OPEN");
-            }
-            
+						if (  (header.indexOf("GET /open") >= 0) && doorClosed == LOW   )
+						{
+						  activate();
+						  addLogEntry("Client request for OPEN");
+						}
+			   		 
 
-            
-            doorClosed = digitalRead(closedSwitch);
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><a href=\"http://");
-            client.println(myIP);
-            client.println("/\"><h1>BROADLEAF GARAGE SERVER</a></h1>");
-            
-            if ( doorClosed == LOW ) { client.println("<body><h3>Currently CLOSED</h3>"); }
+						// Get the switches (as there may have been an activate that has occurred
+						doorClosed = digitalRead(closedSwitch);
+						doorOpen = digitalRead(openSwitch);
 
-            else if ( doorOpen == LOW ) { client.println("<body><h3>Currently OPEN</h3>"); }
+						sendPageHead(client);
+						
+						if (header.indexOf("GET /test") >= 0) 
+						{
+							
+							client.println("Sending Test signal to Mnemonics");
+							updateWeb( remote_PAGEDEST, "TESTING" );
+							addLogEntry("Testing Mnemonics Connection");
+							sendPageFoot(client);
 
-            else { client.println("<body><h3>Currently MOVING</h3>"); }
-           
-           
-           if ( doorClosed == HIGH ) { client.println("<p>Switch1 = OPEN<p>"); }
-           else { client.println("<p>Switch1 = CLOSED<p>"); }
+							// Break out of the while loop
+							break;
+						}
+						
+						// Check switches for current state
+						if ( doorClosed == LOW ) { client.println("<h3>Currently CLOSED</h3>"); }
+						else if ( doorOpen == LOW ) { client.println("<h3>Currently OPEN</h3>"); }
+						else { client.println("<h3>Currently MOVING</h3>"); }
 
-           if ( doorOpen == HIGH ) { client.println("<p>Switch2 = OPEN<p>"); }
-           else { client.println("<p>Switch2 = CLOSED<p>"); }
-            
-            client.println("<p><a href=\"/pulse\"><button class=\"button\">PULSE</button></a></p>");
-            client.println("<p><a href=\"/open\"><button class=\"button\">OPEN</button></a></p>");
-            client.println("<p><a href=\"/close\"><button class=\"button\">CLOSE</button></a></p>");
-            
-            if (  (header.indexOf("GET /logs") >= 0) )
-            {
-            for ( int i = 0; i < LOGSIZE; i++ )
-            {
-              client.println("<p>Output: ");
-                client.println(currentLogPtr[currentLog].output);
-                client.println(" - ");
-                client.println(currentLogPtr[currentLog].timeStamp);
-                client.println("<p>");
-                
-                currentLog--;
-                if ( currentLog <= -1 )
-                  currentLog = LOGSIZE-1 ;
-            }
-             
-            }
-            else
-            {
-              client.println("<p>Last Output: ");
-              client.println(currentLogPtr[currentLog].output);
-              client.println("</p>");
-              client.println("<p>");
-              client.println(currentLogPtr[currentLog].timeStamp);
-              client.println("<p>");
-              
-              client.println("<p><a href=\"http://");
-              client.println(myIP);
-              client.println("/logs/\">View with all logs</a></p>");
-              
-              
-          }
-            client.println("</body></html>");
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
+						// Report on Switch 1
+						if ( doorClosed == HIGH ) { client.println("<p>Switch1 = OPEN<p>"); }
+						else { client.println("<p>Switch1 = CLOSED<p>"); }
+						// Report on switch 2
+						if ( doorOpen == HIGH ) { client.println("<p>Switch2 = OPEN<p>"); }
+						else { client.println("<p>Switch2 = CLOSED<p>"); }
+			    
+			    		// Add the Action Buttons
+						client.println("<p><a href=\"/pulse\"><button class=\"button\">PULSE</button></a></p>");
+						client.println("<p><a href=\"/open\"><button class=\"button\">OPEN</button></a></p>");
+						client.println("<p><a href=\"/close\"><button class=\"button\">CLOSE</button></a></p>");
+						client.println("<p><a href=\"/test\"><button class=\"button\">TEST COMMS</button></a></p>");
+			    
+			    		// Now decide if the logs are being shown
+						if (  (header.indexOf("GET /logs") >= 0) )
+						{
+							for ( int i = 0; i < LOGSIZE; i++ )
+							{
+								client.println("<p>Output: ");
+								client.println(currentLogPtr[currentLog].output);
+								client.println(" - ");
+								client.println(currentLogPtr[currentLog].timeStamp);
+								client.println("<p>");
 
-  }
+								currentLog--;
+								// check if we need to wrap round
+								if ( currentLog <= -1 )
+								  currentLog = LOGSIZE-1 ;
+							}
+						 
+						}
+						// Otherwise only show the last log entry
+						else
+						{
+							client.println("<p>Last Output: ");
+							client.println(currentLogPtr[currentLog].output);
+							client.println("</p>");
+							client.println("<p>");
+							client.println(currentLogPtr[currentLog].timeStamp);
+							client.println("<p>");
+
+							client.println("<p><a href=\"http://");
+							client.println(myIP);
+							client.println("/logs/\">View with all logs</a></p>");
+						  
+						  
+						}
+			  
+						sendPageFoot(client);
+
+						// Break out of the while loop
+						break;
+			  		} 
+			  		else // if you got a newline, then clear currentLine
+			  		{ 
+			    		currentLine = "";
+			  		}
+				} 
+				else if (c != '\r') // if you got anything else but a carriage return character,
+				{  
+			  		currentLine += c;      // add it to the end of the currentLine
+				}
+			}
+	    }
+		// Clear the header variable
+		header = "";
+		// Close the connection
+		client.stop();
+
+  	}
 }
 
 
@@ -554,4 +556,61 @@ void updateWeb(String pageDestination, String dataToSend)
 	addLogEntry(line);
 
 	sender.stop();
+}
+
+/**
+ * Name: sendPageHead
+ * 
+ * Return: None
+ * 
+ * Parameters: Wifi Client
+ * 
+ * Description: Bulds the output page
+ *
+ */
+void sendPageHead( WiFiClient client )
+{
+	// HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+    // and a content-type so the client knows what's coming, then a blank line:
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-type:text/html");
+    client.println("Connection: close");
+    client.println();
+    
+    // Display the HTML web page
+    client.println("<!DOCTYPE html><html>");
+    client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+    client.println("<link rel=\"icon\" href=\"data:,\">");
+    // CSS to style the on/off buttons 
+    // Feel free to change the background-color and font-size attributes to fit your preferences
+    client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+    client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+    client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+    client.println(".button2 {background-color: #77878A;}</style></head>");
+    
+    // Web Page Heading
+    client.println("<body><a href=\"http://");
+    client.println(myIP);
+    client.println("/\"><h1>BROADLEAF GARAGE SERVER</a></h1>");
+    
+    
+	
+}
+
+
+/**
+ * Name: sendPageFoot
+ * 
+ * Return: None
+ * 
+ * Parameters: Wifi Client
+ * 
+ * Description: Bulds the output page
+ *
+ */
+void sendPageFoot( WiFiClient client )
+{
+	client.println("</body></html>");
+    // The HTTP response ends with another blank line
+    client.println();
 }
